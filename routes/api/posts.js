5d756311d7b1b6cd5d1c5998c159589ba7c2aaff
Matt <boblikesbbq@gmail.com>
2018-11-16 16:24:53 -0800

    Add like an unlike post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -77,4 +77,49 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
 });
 
 
+// @route POST api/posts/like/:id
+// @desc Like Post
+// @access  Private
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+            return res.status(400).json({alreadyliked: "User already likes this post"});
+          }
+          post.likes.unshift({user: req.user.id});
+          post.save().then(post => res.json(post))
+            .catch(err => console.log(`There was a error ${JSON.stringify(err, undefined, 2)}`));
+        })
+    })
+    .catch(err => {
+      console.log(`There was a error ${JSON.stringify(err, undefined, 2)}`)
+      res.status(404).json({postnotfound: "No post found"});
+    });
+});
+
+// @route POST api/posts/unlike/:id
+// @desc Unlike Post
+// @access  Private
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+            return res.status(400).json({notliked: "You have not liked this post"});
+          }
+          const removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
+          post.likes.splice(removeIndex, 1);
+          post.save().then(post => res.json(post))
+            .catch(err => console.log(`There was a error ${JSON.stringify(err, undefined, 2)}`));
+        })
+    })
+    .catch(err => {
+      console.log(`There was a error ${JSON.stringify(err, undefined, 2)}`)
+      res.status(404).json({postnotfound: "No post found"});
+    });
+});
+
 module.exports = router;
