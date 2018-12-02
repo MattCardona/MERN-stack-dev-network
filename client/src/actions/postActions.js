d0f93fd057f1aab72e56ca3993c1af27a5b32423
Matt <boblikesbbq@gmail.com>
2018-12-01 16:40:50 -0800

    Add intial state for Posts Component

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { ADD_POST, GET_ERRORS } from './types.js';
+
+// add a post
+export const addPost = postData => dispatch => {
+  axios.post('/api/posts', postData)
+    .then(res =>
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      });
+    )
+    .catch(e =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: e.response.data
+      })
+    )
+}
