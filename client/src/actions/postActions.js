e0b5d1f53a21c73dc1788ec457aad50d2caed697
Matt <boblikesbbq@gmail.com>
2018-12-03 16:49:19 -0800

    Add final touches to Post component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { ADD_POST, GET_ERRORS, GET_POSTS, GET_POST, POST_LOADING, DELETE_POST, LIKE_POST } from './types.js';
+import { ADD_POST, GET_ERRORS, GET_POSTS, GET_POST, POST_LOADING, DELETE_POST, LIKE_POST, CLEAR_ERRORS } from './types.js';
 
 // add a post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios.post('/api/posts', postData)
     .then(res =>
       dispatch({
@@ -107,7 +108,25 @@ export const removeLike = id => dispatch => {
 
 // add a comment
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios.post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(e =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: e.response.data
+      })
+    )
+}
+
+// delete comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios.delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
@@ -126,5 +145,12 @@ export const addComment = (postId, commentData) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  }
+}
+
+// clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   }
 }
